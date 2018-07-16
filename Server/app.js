8738973592424eb6e5a527667e9eb07ebffda7bb
Xhh <2791341417@qq.com>
2018-07-16 14:05:27 +0800

    ++

--- Server/app.js
+++ Server/app.js
@@ -1,18 +1,27 @@
-﻿// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+﻿const url = require('url');
+
+const ws = require('ws');
+
+const Cookies = require('cookies');
+
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-const templating = require('./templating');
-
-const isProduction = process.env.NODE_ENV === 'production';
+const bodyParser = require('koa-bodyparser');
 
 // 导入controller middleware:
 const controller = require('./controller');
 
-const bodyParser = require('koa-bodyparser');
+const templating = require('./templating');
+
+const WebSocketServer = ws.Server;
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
+// log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     var
@@ -23,20 +32,142 @@ app.use(async (ctx, next) => {
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
+// parse user from cookie:
+app.use(async (ctx, next) => {
+    ctx.state.user = parseUser(ctx.cookies.get('name') || '');
+    await next();
+});
+
+// static file support:
 if (! isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
 
+// parse request body:
 app.use(bodyParser());
 
+// add nunjucks as view:
 app.use(templating('views', {
     noCache: !isProduction,
     watch: !isProduction
 }));
 
+// add controller middleware:
 app.use(controller());
 
 // 在端口3000监听:
-app.listen(3000);
+let server = app.listen(3000);
+
+function parseUser(obj) {
+    if (! obj) {
+        return;
+    }
+    console.log('try parse: ' + obj);
+    let s = '';
+    if (typeof obj === 'string') {
+        s = obj;
+    } else if (obj.headers) {
+        let cookies = new Cookies(obj, null);
+        s = cookies.get('name');
+    }
+    if (s) {
+        try {
+            let user = JSON.parse(Buffer.from(s, 'base64').toString());
+            console.log(`User: ${user.name}, ID: ${user.id}`);
+            return user;
+        } catch (e) {
+            // ignore
+        }
+    }
+}
+
+function createWebSocketServer(server, onConnection, onMessage, onClose, onError) {
+    let wss = new WebSocketServer({
+        server: server
+    });
+    wss.broadcast = function broadcast(data) {
+        wss.clients.forEach(function each(client) {
+            client.send(data);
+        });
+    };
+
+    onConnection = onConnection || function () {
+        console.log('[WebSocket] connected.');
+    };
+
+    onMessage = onMessage || function (msg) {
+        console.log('[WebSocket] message received: ' + msg);
+    };
+
+    onClose = onClose || function (code, message) {
+        console.log(`[WebSocket] closed: ${code} - ${message}`);
+    };
+
+    onError = onError || function (err) {
+        console.log('[WebSocket] error: ' + err);
+    };
+    
+    wss.on('connection', function (ws) {
+        let location = url.parse(ws.upgradeReq.url, true);
+        console.log('[WebSocketServer] connection: ' + location.href);
+        ws.on('message', onMessage);
+        ws.on('close', onClose);
+        ws.on('error', onError);
+        if (location.pathname !== '/ws/chat') {
+            // close ws:
+            ws.close(4000, 'Invalid URL');
+        }
+        // check user:
+        let user = parseUser(ws.upgradeReq);
+        if (!user) {
+            ws.close(4001, 'Invalid user');
+        }
+        ws.user = user;
+        ws.wss = wss;
+        onConnection.apply(ws);
+    });
+    console.log('WebSocketServer was attached.');
+    return wss;
+}
+
+var messageIndex = 0;
+
+function createMessage(type, user, data) {
+    messageIndex ++;
+    return JSON.stringify({
+        id: messageIndex,
+        type: type,
+        user: user,
+        data: data
+    });
+}
+
+function onConnect() {
+    let user = this.user;
+    let msg = createMessage('join', user, `${user.name} joined.`);
+    this.wss.broadcast(msg);
+    // build user list:
+    let users = this.wss.clients.map(function (client) {
+        return client.user;
+    });
+    this.send(createMessage('list', user, users));
+}
+
+function onMessage(message) {
+    console.log(message);
+    if (message && message.trim()) {
+        let msg = createMessage('chat', this.user, message.trim());
+        this.wss.broadcast(msg);
+    }
+}
+
+function onClose() {
+    let user = this.user;
+    let msg = createMessage('left', user, `${user.name} is left.`);
+    this.wss.broadcast(msg);
+}
+
+app.wss = createWebSocketServer(server, onConnect, onMessage, onClose);
+
 console.log('app started at port 3000...');
